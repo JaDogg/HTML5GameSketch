4f158f6d94c894262e35b0b1b4903065c178168e
B. A. Bhathiya N. H. Perera <jadogg.coder user name to domain gmail.com>
2014-04-08 11:12:26 +0530

    First Commit

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : E:\PRJ\Projects\HTML5GameSketch\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
---
+++ platforms/android/ant-gen/R.java.d
@@ -0,0 +1,14 @@
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-gen\com\phonegap\helloworld\R.java \
+ : E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\drawable\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\drawable-hdpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\drawable-ldpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\drawable-mdpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\drawable-xhdpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\values\strings.xml \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\res\xml\config.xml \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-build\res\drawable\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
+E:\PRJ\Projects\HTML5GameSketch\platforms\android\ant-build\AndroidManifest.xml \
